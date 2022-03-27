dbcc57ce962bc213b0cd69a06556a09e25dc1069
MICHAELKITH <michaelkithinji27@gmail.com>
2022-03-27 18:45:41 +0300

    first commit

---
+++ button.js
@@ -0,0 +1,34 @@
+function myFunction(){
+    alert ("Strong proficiency in javascript object model, DOM manipulation and event handlers, data structures, aligorithms, JSX and Babel.");
+}
+function myCall(){
+    alert ("Atleast 1 Year of experience in Node J.S development and I have good knowledge of Node.js Frameworks");
+}
+function myCall1(){
+    alert ("I have specialisied in the design, testing and implemetation of software using PHP");
+}
+function myFunction1(){
+    alert ("I'm expert of design tasks and deep knowledge of core languages (APIs,HTTP-based APIs, LibraryAPIs, jQuery and React and many npm packages out there.");
+}
+function myFunction3(){
+    alert ("SPSS stands for Statistical Package for Social Sciences. I have vast knowledge in analysing complex and time consuming data using SPSS software for more than 3 years.");
+}
+function myFunction4(){
+    alert ("Stata is a coding language for C programming. I have been using this powerful software to analyze and manage graphical visualization of data. It is primarily used by researchers in the field of economics, biomedicine, and political science to examine data");
+}
+function myFunction5(){
+    alert ("I have a strong understanding of graphical principles this inculdes ideation,Branding,Typography,UI and UX design ")
+}
+function myFunction6(){
+    alert ("STRONG DBA Experience - MongoDB. Strong scripting experience-Unix/shell and Technical knowledge of Linux and Window Based platforms")
+}
+function myFunction7(){
+    alert ("I have good grasp of Asynchronous Programming and its Workarounds")
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
